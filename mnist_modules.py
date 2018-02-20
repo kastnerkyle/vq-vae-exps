5b1f675810777d68fcccd6439356ab5a343d0bfc
Rithesh Kumar <ritheshkumar.95@gmail.com>
2018-02-20 14:36:41 -0500

    vq-vae works beautifully!

--- mnist_modules.py
+++ mnist_modules.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+from copy import deepcopy
+
+
+def to_scalar(arr):
+    if type(arr) == list:
+        return [x.cpu().data.tolist()[0] for x in arr]
+    else:
+        return arr.cpu().data.tolist()[0]
 
 
 def to_cuda_variable(array, requires_grad=True, volatile=False):
@@ -20,7 +28,7 @@ class ResidualBlock(nn.Module):
             nn.ReLU(True),
             nn.Conv2d(dim, dim, 1, 1)
         )
-    
+
     def forward(self, x):
         return x + self.block(x)
 
@@ -36,72 +44,65 @@ class AutoEncoder(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(True),
             nn.Conv2d(32, 64, 1, 1, 0),
-            nn.BatchNorm2d(64)
+            nn.BatchNorm2d(64),
+            # nn.ReLU(True)
         )
 
-        self.embedding = nn.Embedding(1024, 64)
+        self.embedding = nn.Embedding(512, 64)
+        self.embedding.weight.data.copy_(1./512 * torch.randn(512, 64))
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.Conv2d(64, 32, 1, 1, 0),
             nn.BatchNorm2d(32),
             nn.ReLU(True),
             nn.ConvTranspose2d(32, 16, 4, 2, 1),
             nn.BatchNorm2d(16),
             nn.ReLU(True),
-            nn.Conv2d(16, 1, 3, 1, 1)
-            # nn.Sigmoid()
+            nn.ConvTranspose2d(16, 1, 4, 2, 1),
+            nn.Sigmoid()
         )
 
     def forward(self, x):
         z_e_x = self.encoder(x)
         B, C, H, W = z_e_x.size()
 
-        z_e_x_transp = z_e_x.transpose(1, 2).transpose(2, 3) # (B, H, W, C)
-        emb = self.embedding.weight.transpose(0, 1)
-        dists = torch.pow(z_e_x_transp.unsqueeze(4)-emb[None, None, None], 2).sum(-2)
+        z_e_x_transp = z_e_x.permute(0, 2, 3, 1)  # (B, H, W, C)
+        emb = self.embedding.weight.transpose(0, 1)  # (C, K)
+        dists = torch.pow(
+            z_e_x_transp.unsqueeze(4) - emb[None, None, None],
+            2
+        ).sum(-2)
         latents = dists.min(-1)[1]
 
+        # z_q_x = self.embedding(latents.view(latents.size(0), -1))
         z_q_x = self.embedding(latents.view(latents.size(0), -1))
-        z_q_x = z_q_x.view(B, H, W, C).transpose(2, 3).transpose(1, 2)
-        x_tilde = self.decoder(z_q_x)
-        return x_tilde, z_e_x, z_q_x
-
-
-class NPAutoEncoder(nn.Module):
-    def __init__(self):
-        super(NPAutoEncoder, self).__init__()
-        self.encoder = nn.Embedding(60032, 7*7*64)
-        self.embedding = nn.Embedding(512, 64)
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 4, 2, 1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, 4, 2, 1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            nn.Conv2d(16, 1, 3, 1, 1),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        z_e_x = self.encoder(x).view(-1, 64, 7, 7)
-        B, C, H, W = z_e_x.size()
-
-        z_e_x_transp = z_e_x.transpose(1, 2).transpose(2, 3)  # (B, H, W, C)
-        emb = self.embedding.weight.transpose(0, 1)
-        dists = torch.pow(z_e_x_transp.unsqueeze(
-            4) - emb[None, None, None], 2).sum(-2)
-        latents = dists.min(-1)[1]
-
-        z_q_x = self.embedding(latents.view(latents.size(0), -1))
-        z_q_x = z_q_x.view(B, H, W, C).transpose(2, 3).transpose(1, 2)
+        z_q_x = z_q_x.view(B, H, W, C).permute(0, 3, 1, 2)
         x_tilde = self.decoder(z_q_x)
         return x_tilde, z_e_x, z_q_x
 
 
 if __name__ == '__main__':
-    x = torch.randn(32, 3, 28, 28).cuda()
-    x = Variable(x)
-
     model = AutoEncoder().cuda()
-    model(x)
+    model.zero_grad()
+    x = Variable(torch.randn(32, 1, 28, 28).cuda(), requires_grad=False)
+    x_tilde, z_e_x, z_q_x = model(x)
+    z_q_x.retain_grad()
+
+    loss_1 = F.binary_cross_entropy(x_tilde, x)
+    loss_1.backward(retain_graph=True)
+    assert model.encoder[-2].bias.grad is None
+    model.embedding.zero_grad()
+    z_e_x.backward(z_q_x.grad, retain_graph=True)
+    assert model.embedding.weight.grad.sum().data.cpu().numpy()[0] == 0
+    bias = deepcopy(model.encoder[-2].bias.grad.data)
+
+    loss_2 = F.mse_loss(z_q_x, z_e_x.detach())
+    loss_2.backward(retain_graph=True)
+    emb = deepcopy(model.embedding.weight.grad.data)
+    assert (bias == model.encoder[-2].bias.grad.data).all() is True
+
+    loss_3 = 0.25*F.mse_loss(z_e_x, z_q_x.detach())
+    loss_3.backward()
+    assert (emb == model.embedding.weight.grad.data).all() is True
+
+    print loss_1, loss_2, loss_3
--- test_mnist.py
+++ test_mnist.py
@@ -1,22 +1,23 @@
 import torch
 import torch.nn.functional as F
 from torchvision import datasets, transforms
-from mnist_modules import AutoEncoder
+from mnist_modules import AutoEncoder, to_scalar
 from torch.autograd import Variable
 import numpy as np
 from torchvision.utils import save_image
+import time
 
 
 kwargs = {'num_workers': 1, 'pin_memory': True}
 train_loader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST(
+    datasets.MNIST(
         './data/fashion_mnist/', train=True, download=True,
         transform=transforms.ToTensor()
-        ), batch_size=128, shuffle=False, **kwargs
+        ), batch_size=64, shuffle=False, **kwargs
     )
 
 test_loader = torch.utils.data.DataLoader(
-    datasets.FashionMNIST(
+    datasets.MNIST(
         './data/fashion_mnist/', train=False,
         transform=transforms.ToTensor()
     ), batch_size=32, shuffle=False, **kwargs
@@ -24,45 +25,41 @@ test_loader = torch.utils.data.DataLoader(
 test_data = list(test_loader)
 
 model = AutoEncoder().cuda()
-opt = torch.optim.Adam(model.parameters(), lr=2e-4)
-
-
-def to_scalar(arr):
-    if type(arr) == list:
-        return [x.cpu().data.tolist()[0] for x in arr]
-    else:
-        return arr.cpu().data.tolist()[0]
+opt = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 
 def train(epoch):
     train_loss = []
     for batch_idx, (data, _) in enumerate(train_loader):
+        start_time = time.time()
         x = Variable(data, requires_grad=False).cuda()
-        x_tilde, z_e_x, z_q_x = model(x)
-
-        z_q_x.retain_grad()
-
-        loss_recons = F.l1_loss(x_tilde, x)
-        # loss_recons = F.binary_cross_entropy(x_tilde, x)
-        loss_e1 = torch.pow(z_e_x.detach() - z_q_x, 2).sum(1).mean()
-        loss_e2 = torch.pow(z_e_x - z_q_x.detach(), 2).sum(1).mean()
 
         opt.zero_grad()
-        loss_recons.backward(retain_graph=True)
-        z_e_x.backward(z_q_x.grad.data, retain_graph=True)
-        (loss_e1 + 0.25*loss_e2).backward()
 
-        # for param in model.parameters():
-        #     param.grad.data.clamp_(-1., 1.)
+        x_tilde, z_e_x, z_q_x = model(x)
+        z_q_x.retain_grad()
 
+        loss_1 = F.binary_cross_entropy(x_tilde, x)
+        # loss_1 = F.l1_loss(x_tilde, x)
+        loss_1.backward(retain_graph=True)
+        model.embedding.zero_grad()
+        z_e_x.backward(z_q_x.grad, retain_graph=True)
+
+        loss_2 = F.mse_loss(z_q_x, z_e_x.detach())
+        loss_2.backward(retain_graph=True)
+
+        loss_3 = 0.25 * F.mse_loss(z_e_x, z_q_x.detach())
+        loss_3.backward()
         opt.step()
 
-        train_loss.append(to_scalar([loss_recons, loss_e1]))
+        train_loss.append(to_scalar([loss_1, loss_2]))
 
-        print 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {}'.format(
+        print 'Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {} Time: {}'.format(
             epoch, batch_idx * len(data), len(train_loader.dataset),
             100. * batch_idx / len(train_loader),
-            np.asarray(train_loss).mean(0))
+            np.asarray(train_loss).mean(0),
+            time.time() - start_time    
+        )
 
 
 def test():
